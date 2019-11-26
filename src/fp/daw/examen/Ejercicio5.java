a5a37d29d12b305e3d83f2b1b930d508c5759758
alumno <alumno@A209P14.informatica.cifpaviles.pa>
2019-11-26 17:37:00 +0100

    Ejemplo 5 resuelto en clase 26-11-2019

--- src/fp/daw/examen/Ejercicio5.java
+++ src/fp/daw/examen/Ejercicio5.java
@@ -1,5 +1,7 @@
 package fp.daw.examen;
 
+import java.util.Scanner;
+
 public class Ejercicio5 {
 
 	/* 
@@ -23,8 +25,60 @@ public class Ejercicio5 {
 	 */
 	
 	public static void main(String[] args) {
+	
+	int cantidad;
+	Scanner input = new Scanner(System.in);
+	System.out.print("Introduce la cantidad de euros: ");
+	cantidad =input.nextInt();
+	while (cantidad > 1){
 		
+		if(cantidad >=500) {
+		System.out.printf("%d billetes de 500\n", cantidad / 500);
+		cantidad = cantidad % 500;
+		}
+		
+		if(cantidad >=200) {
+			System.out.printf("%d billetes de 200\n", cantidad / 200);
+			cantidad = cantidad % 200;
+			}
 
+		if(cantidad >=100) {
+			System.out.printf("%d billetes de 100\n", cantidad / 100);
+			cantidad = cantidad % 100;
+			}
+
+		if(cantidad >=50) {
+			System.out.printf("%d billetes de 50\n", cantidad / 50);
+			cantidad = cantidad % 50;
+			}
+
+		if(cantidad >=20) {
+			System.out.printf("%d billetes de 20\n", cantidad / 20);
+			cantidad = cantidad % 20;
+			}
+	
+	
+		if(cantidad >=10) {
+			System.out.printf("%d billetes de 10\n", cantidad / 10);
+			cantidad = cantidad % 10;
+			}
+	
+		if(cantidad >=5) {
+			System.out.printf("%d billetes de 5\n", cantidad / 5);
+			cantidad = cantidad % 5;
+			}
+	
+		if(cantidad >=2) {
+			System.out.printf("%d monedas de 2\n", cantidad / 2);
+			cantidad = cantidad % 2;
+			}
+	
+		} // acabo el while
+	
+		if (cantidad == 1) {
+		
+		System.out.println("1 moneda de 1 euro");
+
+		}
 	}
-
 }
